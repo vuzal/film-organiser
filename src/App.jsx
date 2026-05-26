ae504cbf7bd147a27cb721d8e3d191262b73b605
Vusal Abbasov <vusalabbasovredmi9@gmail.com>
2026-05-26 18:01:30 +0400

    App changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from "./pages/Home";
-import ListPage from "./pages/ListPage";
 import MyList from "./pages/MyList";
 import './App.css';
 
 export default function App() {
   return (
-      <div className="app-container">
-        <header className="app-header">
-          <Link to="/" className="logo-link">
-            <h1>Film Organiser</h1>
-          </Link>
-          <Link to="/my-lists" className="my-lists-btn">
-            My Lists 📂
-          </Link>
-        </header>
-
-        <main className="app-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/my-lists" element={<MyList />} />
-            <Route path="/list/:id" element={<ListPage />} />
-          </Routes>
-        </main>
-      </div>
+    <div className="app-container">
+      <header className="app-header">
+          <h1>Movie</h1>
+      </header>
+      <main className="app-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/my-lists" element={<MyList />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
